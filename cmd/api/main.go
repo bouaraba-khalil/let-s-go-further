d0ee0a74d6a277546b9511a247c44a6fb60aa57c
khalil <bouaraba.khalil@gmail.com>
2024-10-18 17:55:16 +0200

    chapter 6

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -34,8 +34,8 @@ func main() {
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-	//flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
-	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgresql://618co9:xau_mPxxVUbq1Vf1uIvmtmDv5Lh9YW1eFAQQ1@us-east-1.sql.xata.sh/greenlight:main?sslmode=require", "PostgreSQL DSN")
+
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
